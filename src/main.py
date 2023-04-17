30d6f9d8b5f151b016ececa29bb12b5ab06910cf
problemaprod <problemaprod@gmail.com>
2023-04-17 18:47:17 +0100

    написаны все функции , и вызваны в файле main.py

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from utils import give_operation
+from utils import last_five_operation
+from utils import from_operation
+from utils import to_operation
+
+give_operation(last_five_operation())
--- src/utils.py
+++ src/utils.py
@@ -6,14 +6,47 @@ def last_five_operation():
     with open("operation.json", 'r') as file:
         read_file = json.load(file)
         read_file.reverse()
-        get_file = read_file[0:5]
+        executed_list = []
+        for i in read_file:
+            if 'state' in i.keys():
+                if i['state'] == 'EXECUTED':
+                    executed_list.append(i)
+                else:
+                    continue
+            else:
+                continue
+        get_file = executed_list[0:5]
         get_file.reverse()
         return get_file
 
 
 def give_operation(get_file):
     for get_dict in get_file:
-        print(get_dict.get("date"))
+        get_date = datetime.datetime.strptime(get_dict['date'], '%Y-%m-%dT%H:%M:%S.%f')
+        print(f"{get_date.strftime('%d.%m.%Y')} {get_dict['description']}")
+        print(f"{from_operation(get_dict)} -> {to_operation(get_dict)} ")
+        print(f"{get_dict['operationAmount']['amount']} {get_dict['operationAmount']['currency']['name']}\n")
 
 
-print(give_operation(last_five_operation()))
+def from_operation(operation):
+    if 'from' in operation.keys():
+        split_from = operation['from'].split(' ')
+        if split_from[0] == 'Счет':
+            return f"{split_from[0]} **{split_from[1][-4:]}"
+        else:
+            return f"{split_from[0]} {split_from[1][0:4]} {split_from[1][5:7]}** **** {split_from[1][-4:]}"
+    else:
+        return ''
+
+
+def to_operation(operation):
+    split_to = operation['to'].split(' ')
+    if split_to[0] == 'Счет':
+        return f"{split_to[0]} **{split_to[1][-4:]}"
+    else:
+        return f"{split_to[0]} {split_to[1][0:4]} {split_to[1][5:7]}** **** {split_to[1][-4:]}"
+
+
+# print(last_five_operation())
+# print(give_operation(last_five_operation()))
+# print(card_number(last_five_operation()))
